97e4d258135a4da6b79bcf98c5423fedb626a969
Chad Rappleyea <crrappleyea@gmail.com>
2016-10-31 16:32:35 -0400

    Update fresh_tomatoes.py

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -125,7 +125,7 @@ main_page_content = '''
 
 
 # A single movie entry html template
-# The Movie storyline header below was added by Chad Rappleyea
+# The movie storyline header below was added by Chad Rappleyea
 movie_tile_content = '''
     <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
         <img src="{poster_image_url}" width="220" height="342">
